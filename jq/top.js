b6295cafd1dc7ae52c9f439df6eec5114fb65bcf
KumaHeika <w87502047@gmail.com>
2015-02-16 17:44:12 +0800

    2015-02-16 add top menu

--- jq/top.js
+++ jq/top.js
@@ -7,8 +7,14 @@ $(document).ready(function(){
 	$(window).scroll(function(){
 		if( $(this).scrollTop() > 400 ){
 			$('.top').fadeIn('fast');
+			$('.nav-stly').addClass('nav-stly-fixed');
+			$('.nav-stly').animate({'top':'0px'},500);
 		} else {
 			$('.top').stop().fadeOut('fast');
+			$('.nav-stly').animate({'top':'-120px'},500,function(){
+				$('.nav-stly').removeClass('nav-stly-fixed');
+			});
+			
 		}
 	});
 });
