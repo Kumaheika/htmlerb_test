2c281e9de05c22f865c5ed151e428c3f30182d47
kuma <w87502047@gmail.com>
2015-02-18 01:17:01 +0800

    2012-02-18 old

--- jq/top.js
+++ jq/top.js
@@ -4,22 +4,15 @@ $(document).ready(function(){
 			scrollTop: 0
 		},400);
 	});
-<<<<<<< HEAD
 	$(window).scroll(function(){
 		if( $(this).scrollTop() > 400 ){
-			$('.top').fadeIn('fast');
-			$('.nav-stly').addClass('nav-stly-fixed').slideToggle();
+			$('.top').stop(true).fadeIn('fast');
 		} else {
 			$('.top').stop(true).fadeOut('fast');
-			$('.nav-stly').slideUp(300,function(){
-				$(this).removeClass('nav-stly-fixed').addClass('nav-stly');
-			});
 		}
-=======
+	});
 	//手機模式 收放Menu
 	$('.nav-icon').click(function(){
 		$('ul.navbar').toggleClass('nav-icon-show');
->>>>>>> da30fecd61c4615bec7263ffbff269c853fa87e1
 	});
-	
 });
