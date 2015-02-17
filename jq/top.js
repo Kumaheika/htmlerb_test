a4d42f9faf80fe493b314b73725aa110036a285b
KumaHeika <w87502047@gmail.com>
2015-02-17 11:06:57 +0800

    2015-02-17- add RWD

--- jq/top.js
+++ jq/top.js
@@ -4,17 +4,7 @@ $(document).ready(function(){
 			scrollTop: 0
 		},400);
 	});
-	$(window).scroll(function(){
-		if( $(this).scrollTop() > 400 ){
-			$('.top').fadeIn('fast');
-			$('.nav-stly').addClass('nav-stly-fixed');
-			$('.nav-stly').animate({'top':'0px'},500);
-		} else {
-			$('.top').stop().fadeOut('fast');
-			$('.nav-stly').animate({'top':'-120px'},500,function(){
-				$('.nav-stly').removeClass('nav-stly-fixed');
-			});
-			
-		}
+	$('.nav-stly').each(function(){
+		
 	});
 });
